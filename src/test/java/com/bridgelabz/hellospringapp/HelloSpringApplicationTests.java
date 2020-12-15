64ad4fb1589eb34c72c035dbb4f287d797179fb1
madhuvayya <madhu.vayya@gmail.com>
2020-12-15 18:49:07 +0530

    Initialized project setup

---
+++ src/test/java/com/bridgelabz/hellospringapp/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellospringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
